878cc39c82093cd046b24954f06cbcf76f7b11b7
ozipi <ozipi.nomad@gmail.com>
2016-03-07 01:13:45 -0600

    [style] added jscs/jshint rules and class style

--- src/ipv4-to-bin.js
+++ src/ipv4-to-bin.js
@@ -1,14 +1,14 @@
 var tobinary = require('to-binary');
 
 function ipv4ToBin(ip) {
-  var ipSplit = ip.split(".");
-  var result = [];
-  for (var i = 0; i < ipSplit.length; i++) {
-	  var binarySegment = tobinary(ipSplit[i]);
-	  result.push(binarySegment);
-  }
+	var ipSplit = ip.split(".");
+	var result = [];
+	for (var i = 0; i < ipSplit.length; i++) {
+		var binarySegment = tobinary(ipSplit[i]);
+		result.push(binarySegment);
+	}
 
-  return result.join(".");
+	return result.join(".");
 }
 
 module.exports = ipv4ToBin;
--- test/ipv4-to-bin.js
+++ test/ipv4-to-bin.js
@@ -1,12 +1,12 @@
 var should = require('chai').should(),
-    ipv4ToBin = require('../src/ipv4-to-bin.js');
+	ipv4ToBin = require('../src/ipv4-to-bin.js');
 
 describe('#ipv4-to-bin', function() {
-  it('converts 127.0.0.1 into 01111111.00000000.00000000.00000001;', function() {
-    ipv4ToBin('127.0.0.1').should.equal('01111111.00000000.00000000.00000001');
-  });
+	it('converts 127.0.0.1 into 01111111.00000000.00000000.00000001;', function() {
+		ipv4ToBin('127.0.0.1').should.equal('01111111.00000000.00000000.00000001');
+	});
 
-  it('converts 192.168.0.1 into 11000000.10101000.00000000.00000001;', function() {
-    ipv4ToBin('192.168.0.1').should.equal('11000000.10101000.00000000.00000001');
-  });
+	it('converts 192.168.0.1 into 11000000.10101000.00000000.00000001;', function() {
+		ipv4ToBin('192.168.0.1').should.equal('11000000.10101000.00000000.00000001');
+	});
 });
